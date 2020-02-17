cd18562231b48d9a9f0f2130ab5a50d9d4d011fa
Christine Simko <christinesimko@me.com>
2020-02-16 16:10:49 -0800

    beautified the js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const fs = require("fs"); 
+const fs = require("fs");
 
 //read input, text file
 
@@ -12,13 +12,13 @@ const inputArr = inputString.split('\n');
 
 const sizeInput = inputArr[0]; // 5 5
 const sizeInputArr = sizeInput.split(" ");
-const xLimit = Number( sizeInputArr[0] ) - 1; // x axis dimension
-const yLimit = Number( sizeInputArr[1] ) - 1; // y axis dimension
+const xLimit = Number(sizeInputArr[0]) - 1; // x axis dimension
+const yLimit = Number(sizeInputArr[1]) - 1; // y axis dimension
 
 const locInput = inputArr[1]; // 1 2
 const locInputArr = locInput.split(" ");
-let xHoo = Number( locInputArr[0] ); // x axis Hoover
-let yHoo = Number( locInputArr[1] ); // y axis Hoover
+let xHoo = Number(locInputArr[0]); // x axis Hoover
+let yHoo = Number(locInputArr[1]); // y axis Hoover
 
 const indDir = inputArr.length - 1;
 
@@ -27,41 +27,41 @@ const directions = inputArr[indDir]; //cardinal dirs
 const dirtPatchInputArr = inputArr.slice(2, indDir);
 const dirtPatchesObj = {};
 
-for ( let i = 0; i < dirtPatchInputArr.length; i++) {
-	const key = dirtPatchInputArr[ i ];
+for (let i = 0; i < dirtPatchInputArr.length; i++) {
+    const key = dirtPatchInputArr[i];
 
-	dirtPatchesObj[ key ] = true;
+    dirtPatchesObj[key] = true;
 }
 
 //main logic
 
 let count = 0;
 
-for ( let i = 0; i < directions.length; i++ ) {
-  const dir = directions[ i ];
-  // Position of Hoover is xHoo, yHoo
-  // to move Hoover, update xHoo and yHoo
-  // Updates in real time, labeled
-  if ( dir === 'N' && yHoo < yLimit ) {
-  	yHoo++;
-  }
-  if ( dir === 'S' && yHoo > 0 ) {
-  	yHoo--;
-  }
-  if ( dir === 'W' && xHoo > 0 ) {
-  	xHoo--;
-  }
-  if ( dir === 'E' && xHoo < xLimit ) {
-  	xHoo++;
-  }
-//equating cardinal dir to x and y axis locale
-  
-  const currentPosStr = xHoo + ' ' + yHoo; // Checking current position to see if it is dirt
+for (let i = 0; i < directions.length; i++) {
+    const dir = directions[i];
+    // Position of Hoover is xHoo, yHoo
+    // to move Hoover, update xHoo and yHoo
+    // Updates in real time, labeled
+    if (dir === 'N' && yHoo < yLimit) {
+        yHoo++;
+    }
+    if (dir === 'S' && yHoo > 0) {
+        yHoo--;
+    }
+    if (dir === 'W' && xHoo > 0) {
+        xHoo--;
+    }
+    if (dir === 'E' && xHoo < xLimit) {
+        xHoo++;
+    }
+    //equating cardinal dir to x and y axis locale
 
-  if ( dirtPatchesObj[ currentPosStr ] ) {
-  	count++;
-  	delete dirtPatchesObj[ currentPosStr ]; // Dirt is cleaned and to keep from iterating over previous patches, you delete them after cleaning
-  }
+    const currentPosStr = xHoo + ' ' + yHoo; // Checking current position to see if it is dirt
+
+    if (dirtPatchesObj[currentPosStr]) {
+        count++;
+        delete dirtPatchesObj[currentPosStr]; // Dirt is cleaned and to keep from iterating over previous patches, you delete them after cleaning
+    }
 
 }
 
@@ -69,8 +69,8 @@ const finalPos = xHoo + ' ' + yHoo;
 const dirtPatchesCleaned = count;
 //convert to desired output format
 
-console.log( finalPos );
-console.log( dirtPatchesCleaned );
+console.log(finalPos);
+console.log(dirtPatchesCleaned);
 //give output with goals in mind; 
 // 1. final position of Roomba/Hoover
 // 2. number of dirt patches cleaned
